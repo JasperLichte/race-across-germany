382ee7a7e0c2bf76b11fec98a10e0798b1a8e23f
Jasper Lichte <jasper.lichte@prgh-group.com>
2022-12-20 23:17:37 +0100

    Add partner logos

---
+++ src/components/PartnerBanner/PartnerBanner.js
@@ -0,0 +1,22 @@
+import './PartnerBanner.scss';
+import depressionshilfe from './logos/deutscheDepressionsHilfe.png';
+import dfwi from './logos/dfwi.svg';
+import kickstart from './logos/kickstart-in-it.svg';
+import velopunkt from './logos/velopunkt.png';
+import noack from './logos/noack-sport-support.png';
+import maxplaner from './logos/maxplaner.png';
+
+function PartnerBanner() {
+  return (
+    <div className='PartnerBanner'>
+        <a href='https://www.kickstart-in.it/'><img src={kickstart} className='kickstart' alt='KICKstart in IT' /></a>  
+        <a href='https://www.maxplaner.com/'><img src={maxplaner} className='maxplaner' alt='Max Planer' /></a>
+        <a href='https://velopunkt.com/'><img src={velopunkt} className='velopunkt' alt='Velo.' /></a>
+        <a href='https://www.dfwi.de/'><img src={dfwi} className='dfwi' alt='DFWI'/></a>
+        <a href='https://noacksportsupport.de/'><img src={noack} className='noack' alt='Noack Sport Support' /></a>
+        <a href='https://www.deutsche-depressionshilfe.de/start'><img src={depressionshilfe} className='depressionshilfe' alt='Deutsche Depressionshilfe' /></a>
+    </div>
+  );
+}
+
+export default PartnerBanner;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Header from './components/Header/Header'
 import RaceDescriptionSection from './components/RaceDescriptionSection/RaceDescriptionSection';
+import PartnerBanner from './components/PartnerBanner/PartnerBanner';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <RaceDescriptionSection />
+    <PartnerBanner />
   </React.StrictMode>
 );
