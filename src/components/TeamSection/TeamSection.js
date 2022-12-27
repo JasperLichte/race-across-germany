8f6c2bfc3beb0d61abbfd9c409ea732be7c88981
Jasper Lichte <jasper.lichte@prgh-group.com>
2022-12-27 22:24:51 +0100

    Add social links

--- src/components/TeamSection/TeamSection.js
+++ src/components/TeamSection/TeamSection.js
@@ -23,38 +23,73 @@ function TeamSection() {
             <div>
                 <p>Jasper Lichte</p>
                 <span>Athlete</span>
+                <div className='socials'>
+                    <a className='instagram' href="https://www.instagram.com/jasper.cycling">instagram</a>
+                    <a className='strava' href="https://www.strava.com/athletes/40292803">strava</a>
+                    <a className='mail' href="mailto:jasper@lichte.info">mail</a>
+                </div>
             </div>
             <div>
                 <p>Martin Duscha</p>
                 <span>Team manager & organizer</span>
+                <div className='socials'>
+                    <a className='instagram' href="https://www.instagram.com/maaadinhh">instagram</a>
+                    <a className='strava' href="https://www.strava.com/athletes/21472500">strava</a>
+                    <a className='mail' href="mailto:martin@velopunkt.com">mail</a>
+                </div>
             </div>
             <div>
                 <p>Phillip Wieckowski</p>
                 <span>Bike mechanic</span>
+                <div className='socials'>
+                    <a className='instagram' href="https://www.instagram.com/phillip_auf_bike">instagram</a>
+                </div>
             </div>
             <div>
                 <p>Merle Neitzel</p>
                 <span>Physiotherapist</span>
+                <div className='socials'>
+                    <a className='instagram' href="https://www.instagram.com/merle_n_">instagram</a>
+                </div>
             </div>
             <div>
                 <p>Mona Flathmann</p>
                 <span>Motivation & Navigation</span>
+                <div className='socials'>
+                    <a className='instagram' href="https://www.instagram.com/mona_flathmann">instagram</a>
+                    <a className='strava' href="https://www.strava.com/athletes/52675975">strava</a>
+                </div>
             </div>
             <div>
                 <p>Christian Walter</p>
                 <span>Navigation & Nutrition</span>
+                <div className='socials'>
+                    <a className='instagram' href="https://www.instagram.com/christian.cycling">instagram</a>
+                    <a className='strava' href="https://www.strava.com/athletes/53339058">strava</a>
+                </div>
             </div>
             <div>
                 <p>Johann Schäning</p>
                 <span>Performance & Nutrition</span>
+                <div className='socials'>
+                    <a className='instagram' href="https://www.instagram.com/johannschaning">instagram</a>
+                    <a className='strava' href="https://www.strava.com/athletes/58115319">strava</a>
+                </div>
             </div>
             <div>
                 <p>Josephine Noack</p>
                 <span>Head Coach & Sports Scientist</span>
+                <div className='socials'>
+                    <a className='instagram' href="https://www.instagram.com/josephine.noack">instagram</a>
+                    <a className='strava' href="https://www.strava.com/athletes/24473648">strava</a>
+                </div>
             </div>
             <div>
                 <p>Maximilian Planer</p>
                 <span>Mental Coach</span>
+                <div className='socials'>
+                    <a className='instagram' href="https://www.instagram.com/maxplaner">instagram</a>
+                </div>
             </div>
         </div>
       </div>
