e9c276947ca6694ebdc2dfebf79f7ba586a5cb7d
Jasper Lichte <jasper.lichte@prgh-group.com>
2023-01-26 21:00:17 +0100

    Add slides

--- src/components/MediaSection/slides/DieZweiteLuftSlide.js
+++ src/components/MediaSection/slides/DieZweiteLuftSlide.js
@@ -6,7 +6,24 @@ function DieZweiteLuftSlide() {
         <div className='image'>
             <div className='logo'></div>
         </div>
-        <div className='description'></div>
+        <div className='description'>
+            <p>
+                I had the opportunity to be a guest on the podcast <strong>"Die zweite Luft"</strong> where I had the pleasure of
+                appearing alongside my coach Josephine. We had a great conversation about the mental
+                and physical preparation that goes into competing in a race.
+                <br />
+                We delved into our training routines, the strategies we use to stay
+                motivated and focused, and the importance of having a strong support system. We also discussed the challenges
+                and obstacles that we have faced and how we have overcome them.
+                <br />
+                If you're someone who is interested in learning more about how to prepare for a race, then this episode is definitely for you. You can listen
+                to the episode on <strong>Spotify.</strong><br />
+                I highly recommend giving it a listen, and I hope you enjoy it as much as I did!
+            </p>
+            <div className='ctas'>
+                <a href="https://open.spotify.com/episode/5yxu1dewakdqADRsWAbWP0?si=340bc62a4f5a4e41">Listen on Spotify</a>
+            </div>
+        </div>
     </div>
   );
 }
