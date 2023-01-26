d9ab097207374ad8651cb344b383c8564d39a002
Jasper Lichte <jasper.lichte@prgh-group.com>
2023-01-26 20:03:59 +0100

    Add slides

--- src/components/MediaSection/MediaSection.js
+++ src/components/MediaSection/MediaSection.js
@@ -1,9 +1,17 @@
 import './MediaSection.scss';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import DieZweiteLuftSlide from './slides/DieZweiteLuftSlide';
+import './slides/Slides.scss';
+
 
 function MediaSection() {
   return (
     <section className='MediaSection'>
         <div className='main'>
+            <Slide>
+                <DieZweiteLuftSlide />
+            </Slide>
         </div>
     </section>
   );
---
+++ src/components/MediaSection/slides/DieZweiteLuftSlide.js
@@ -0,0 +1,14 @@
+import './DieZweiteLuftSlide.scss';
+
+function DieZweiteLuftSlide() {
+  return (
+    <div className="custom-slide each-slide-effect DieZweiteLuftSlide">
+        <div className='image'>
+            <div className='logo'></div>
+        </div>
+        <div className='description'></div>
+    </div>
+  );
+}
+
+export default DieZweiteLuftSlide;
