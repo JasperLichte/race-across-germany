5c224b54c54ca941d472c2318d96a5e08f5ffeed
Jasper Lichte <jasper.lichte@prgh-group.com>
2023-01-27 21:07:49 +0100

    Add gtm event

--- src/components/MediaSection/MediaSection.js
+++ src/components/MediaSection/MediaSection.js
@@ -8,10 +8,21 @@ import MoPoSlide from './slides/MoPoSlide';
 
 
 function MediaSection() {
+  const onChange = (from, to) => {window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'Generic Event',
+      event_name: 'media_slider_next',
+      media_slider_next : {
+          from_slide: from,
+          to_slide: to, 
+      }
+    });
+  };
+
   return (
     <section className='MediaSection'>
         <div className='main'>
-            <Slide easing='ease-out' transitionDuration={800}>
+            <Slide easing='ease-out' transitionDuration={800} onChange={onChange}>
                 <DieZweiteLuftSlide />
                 <TageblattSlide />
                 <MoPoSlide />
