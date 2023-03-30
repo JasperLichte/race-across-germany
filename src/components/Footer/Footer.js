21ac8d65f09057e7512e1cc58d4f230899b13525
Jasper Lichte <jasper.lichte@prgh-group.com>
2023-03-30 14:50:45 +0200

    Adjust mail

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <div className='links'>
             <h4>Don't hesitate to get in touch with us!</h4>
             <p>We'd be happy to provide any additional information you may need.</p>
-            <span>Mail: <a href='mailto:jasper@lichte.info'>jasper@lichte.info</a></span>
+            <span>Mail: <a href='mailto:team@lichte.info'>jasper@lichte.info</a></span>
             <span>Instagram: <a href='https://www.instagram.com/jasper.cycling/'>jasper.cycling</a></span>
         </div>
       </div>
