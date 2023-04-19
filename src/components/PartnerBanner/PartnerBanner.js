f6f11af3e83457df814f033da343f67283d0ce67
Jasper Lichte <jasper@lichte.info>
2023-04-19 19:15:18 +0200

    Add webxells

--- src/components/PartnerBanner/PartnerBanner.js
+++ src/components/PartnerBanner/PartnerBanner.js
@@ -5,6 +5,7 @@ import sportimport from './logos/sport-import.svg';
 import h1media from './logos/h1media.png'
 import aerycs from './logos/aerycs.png';
 import mucoff from './logos/mucoff.png';
+import webxells from './logos/webxells.svg';
 
 function PartnerBanner({animation, iconSize}) {
   return (
@@ -12,7 +13,8 @@ function PartnerBanner({animation, iconSize}) {
         <a href='https://noacksportsupport.de/'><img src={noack} className='noack' alt='Noack Sport Support' /></a>
         <a href='https://www.sportimport.de/'><img src={sportimport} className='sportimport' alt='Sport Import' /></a>
         <a href='https://aerycs.de/'><img src={aerycs} className='aerycs' alt='Aerycs' /></a>
-        <a href='https://muc-off.com/'><img src={mucoff} className='mucoff' alt='Aerycs' /></a>
+        <a href='https://muc-off.com/'><img src={mucoff} className='mucoff' alt='Muc-Off' /></a>
+        <a href='https://www.webxells.com/'><img src={webxells} className='webxells' alt='webXells' /></a>
         <a href='https://www.maxplaner.com/'><img src={maxplaner} className='maxplaner' alt='Max Planer' /></a>
         <a href='http://www.h1e.eu/'><img src={h1media} className='h1media' alt='H1Media' /></a>
     </div>
